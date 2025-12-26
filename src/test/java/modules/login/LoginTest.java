d86b30be7aca88f7ee8c5e3c7d5c8b2646f3c0ed
rafaabc <faelsabc21@gmail.com>
2025-12-26 18:18:33 +0100

    add before each and after each hooks

--- src/test/java/modules/login/LoginTest.java
+++ src/test/java/modules/login/LoginTest.java
@@ -1,9 +1,7 @@
 package modules.login;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,16 +11,28 @@ import java.time.Duration;
 @DisplayName("Web tests for the Login module")
 public class LoginTest {
     private WebDriver driver;
+
+    @BeforeEach
+    void beforeEach() {
+        //open the browser
+        WebDriverManager.chromedriver().setup();
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+//        this.driver = new ChromeDriver(options);
+        this.driver = new ChromeDriver();
+        this.driver.manage().window().maximize();
+        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        this.driver.get("http://165.227.93.41/lojinha-web/v2/");
+    }
+
+    @AfterEach
+    void afterEach() {
+        driver.quit();
+    }
+
     @Test
     @DisplayName("When using valid credentials, Then login successfully")
     void loginWithValidCredentials() {
-        //open the browser
-        WebDriverManager.chromedriver().setup();
-        this.driver = new ChromeDriver();
-        this.driver.manage().window().maximize();
-        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        this.driver.get("http://165.227.93.41/lojinha-web/v2/");
-
         //fill username
         driver.findElement(By.cssSelector("label[for='usuario']")).click();
         driver.findElement(By.id("usuario")).sendKeys("julio.lima");
@@ -37,20 +47,11 @@ public class LoginTest {
         //assert the user is logged in
         String message = driver.findElement(By.linkText("Boas vindas, Julio de Lima!")).getText();
         Assertions.assertEquals("Boas vindas, Julio de Lima!", message);
-
-
     }
 
     @Test
     @DisplayName("When using invalid credentials, Then error message displays")
     void loginWithInvalidCredentials() {
-        //open the browser
-        WebDriverManager.chromedriver().setup();
-        this.driver = new ChromeDriver();
-        this.driver.manage().window().maximize();
-        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        this.driver.get("http://165.227.93.41/lojinha-web/v2/");
-
         //fill username
         driver.findElement(By.cssSelector("label[for='usuario']")).click();
         driver.findElement(By.id("usuario")).sendKeys("julio.lima");
@@ -64,6 +65,5 @@ public class LoginTest {
 
         String errorMessage = driver.findElement(By.cssSelector(".rounded")).getText();
         Assertions.assertEquals("Falha ao fazer o login", errorMessage);
-
     }
 }
